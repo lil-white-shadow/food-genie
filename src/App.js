3ef62dfc7f19bd09bb95f2b66839ab4fedced8fe
lil-white-shadow <kru.patel.swe@gmail.com>
2022-12-29 16:52:46 -0600

    add input & buttons, basic styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const title = "FoodGenie";
+  const [input, setInput] = useState('')
+  const [search, setSearch] = useState('')
+
+  function handleInput(event) {
+    setInput(event.target.value)
+  }
+
+  function handleSubmit() {
+    setSearch(input);
+    setInput('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="title">
+        <p>{title}</p>
+        <div className="bulb"></div>
+      </div>
+      <input type="text" onChange={event => handleInput(event)} value={input}/>
+      <div className="buttonContainer">
+        <button type="submit" onClick={handleSubmit}>Search</button>
+        <button>Surpise Me!</button>
+      </div>
+      {input !== '' ? <p>input: {input}</p> : <p>Input: </p>}
+      {search !== '' ? <p>search: {search}</p> : <p>Search: </p>}
     </div>
   );
 }
