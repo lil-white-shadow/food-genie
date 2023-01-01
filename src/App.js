c0eefde67fe04391963cf3535519d2c3f46dace2
lil-white-shadow <kru.patel.swe@gmail.com>
2023-01-01 01:01:10 -0600

    search ingredients

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
+export default function App() {
   const title = "FoodGenie";
   const [input, setInput] = useState('')
   const [search, setSearch] = useState('')
+
+  const [ingredients, setIngredients] = useState('')
+  const [ingredientsSource, setIngredientsSource] = useState('')
+  // const [imageUrl, setImageUrl] = useState('')
+  
+  useEffect(() => {
+    let key = 'AIzaSyBovUDUlWQu_lLCsJHNFQC5AnUJT3vrUdg'
+    let url = "https://www.googleapis.com/customsearch/v1?key=" + key + "&cx=f2a2aa6d87dfc4e07&q="
+    if(search !== '') {
+      fetch(url + search + "&num=1")
+      .then(response => response.json())
+      // .then(response => console.log(response))
+      .then(response => {
+        let snippet = response.items[0].snippet
+        let source = response.items[0].formattedUrl
+        // let imageUrl = response.items[0].formattedUrl
+        setIngredients(snippet)
+        setIngredientsSource(source)
+        // setImageUrl
+      })
+  }
+    console.log(ingredients)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search])
+  
 
   function handleInput(event) {
     setInput(event.target.value)
@@ -12,7 +37,23 @@ function App() {
 
   function handleSubmit() {
     setSearch(input);
-    setInput('');
+    setInput('')
+
+    // setInput('');
+    // console.log(search)
+    // fetch(url + search + "&num=1")
+    // .then(response => response.json())
+    // .then(response => response.items[0].htmlSnippet)
+    // .then(sentence => {
+    //   console.log(sentence)
+    //   if(sentence.indexOf(' is ') < sentence.indexOf(' are ')) {
+    //     setIngredients(sentence.substring(sentence.indexOf(' is') + 3, sentence.indexOf(' are')).replace('.', ',').replace(' and', ',').toLowerCase().replace(/\s*,\s*/g, ',').replace(' and ', ',').replace('<b>', '').replace('</b>', '').split(','))
+    //   } else {
+    //     setIngredients(sentence.substring(sentence.indexOf(' are') + 4, sentence.indexOf(' also')).replace('.', ',').replace(' and', ',').toLowerCase().replace(/\s*,\s*/g, ',').replace(' and ', ',').replace('<b>', '').replace('</b>', '').split(','))
+    //   }
+    //   });
+    // console.log(ingredients)
+
   }
 
   return (
@@ -26,10 +67,21 @@ function App() {
         <button type="submit" onClick={handleSubmit}>Search</button>
         <button>Surpise Me!</button>
       </div>
-      {input !== '' ? <p>input: {input}</p> : <p>Input: </p>}
-      {search !== '' ? <p>search: {search}</p> : <p>Search: </p>}
+      <div className="results">
+        {input !== '' ? <p>Input: {input}</p> : <p>Input: </p>}
+        {search !== '' ? <p>Search: {search}</p> : <p>Search: </p>}
+        {ingredients !== '' ? <p>Ingredients: {ingredients}  <a href={ingredientsSource}>Read more</a>[Source: {ingredientsSource.substring(8, ingredientsSource.indexOf('com') + 3)}]</p> : <p>Ingredients: </p>}
+      </div>
     </div>
   );
 }
 
-export default App;
+
+/* 
+Components
+1. Home - Search input for any dish or ingredient
+2. Home - buttons = ingredients, nutrition, near me
+3. Ingredients - ingredients card
+4. Nutrition - nutrition info card
+5. Near me - google map results for restaurants or stores
+*/
