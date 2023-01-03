495b586592c86407480553c95d37bd061e01c848
lil-white-shadow <kru.patel.swe@gmail.com>
2023-01-02 21:51:16 -0600

    update About content

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 export default function About() {
-  const content = "Have you ever wondered what goes in your food? Or how many calories are in a slice of that pizza you had for lunch? Our founders found themselves constantly trying to find answers to these questions. And then, Food Genie was created."
+  const contentPara1 = "Have you ever wondered what goes in your food? Or how many calories are in a slice of that pizza you had for lunch?"
+
+    const contentPara2 = "Say goodbye to your former self! We have brought Food Genie at your service and you are not limited to just 3 wishes. Search ingredients and nutrition info for any food!"
 
   return (
   <div className="card">
     <div className="cardTitle">About Food Genie</div>
     <div className="cardContent">
-      <p>{content}</p>
+      <p>{contentPara1}</p>
+      <p>{contentPara2}</p>
     </div>
     <div>
     </div>
