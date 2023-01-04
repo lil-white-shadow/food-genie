096b391409c31eeb8118f572cd06f6f2419d43f4
lil-white-shadow <kru.patel.swe@gmail.com>
2023-01-04 12:46:29 -0600

    update nav  alignment

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,7 +8,7 @@ export default function Nav(props) {
         </div>
         : 
         <div className="nav">
-        <div className="logo">Food Genie</div>
+        <button className="logo">FoodGenie</button>
         <button onClick={() => props.goToComponent(props.AboutRef)}>About</button>
         <button onClick={() => props.goToComponent(props.ContactRef)}>Contact</button>
       </div>
