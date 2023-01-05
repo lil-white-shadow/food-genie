67861e6447ff983967ae9f5dea72237ac4319d24
lil-white-shadow <kru.patel.swe@gmail.com>
2023-01-04 18:19:39 -0600

    remove Nav component when search is rendered

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,6 @@
 export default function Nav(props) {
-  let length = props.search.length
   return(
-      length === 0 ? 
       <div className="nav">
-          <button onClick={() => props.goToComponent(props.AboutRef)}>About</button>
-          <button onClick={() => props.goToComponent(props.ContactRef)}>Contact</button>
-        </div>
-        : 
-        <div className="nav">
         <button className="logo">FoodGenie</button>
         <button onClick={() => props.goToComponent(props.AboutRef)}>About</button>
         <button onClick={() => props.goToComponent(props.ContactRef)}>Contact</button>
